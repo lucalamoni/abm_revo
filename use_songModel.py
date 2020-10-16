bbd03504b59930e1909d5f6877b447e5fa7aa31c
Luca Lamoni <lucalamoni@gmail.com>
2020-10-16 15:53:44 +0200

    git ignore, non guardare use_song_model

--- use_songModel.py
+++ use_songModel.py
@@ -1,6 +1,7 @@
-import os
-os.chdir('C:/Users/Luca/ABM_master')
-exec(open('C:/Users/Luca/ABM_master/ABM_14082019_python3_refactored.py').read())
+# import os
+# os.chdir('C:/Users/Luca/ABM_master')
+# exec(open('C:/Users/Luca/ABM_master/ABM_14082019_python3_refactored.py').read())
+from ABM_14082019_python3_refactored import songModel
 
 def use_songModel(filename, FGS, MGS):
 	songModel(filename = filename, FGS = FGS, MGS = MGS)
