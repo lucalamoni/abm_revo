14af5c4b9390d2b39a7f62aeabf94a6a0f8b8e71
Luca Lamoni <lucalamoni@gmail.com>
2021-01-06 17:48:38 +0100

    LR novelty experiments

--- experiment_list.py
+++ experiment_list.py
@@ -5,15 +5,15 @@ import math
 import multiprocessing as mp
 
 
-from ABM_07112020_sol1_random_LR_memory_and_CM_changed import songModel
+from ABM_07112020_sol1_random_LR_memory_and_CM_changed_and_novelty_bias import songModel
 
 def use_songModel(filename, FGS, MGS, modelMode, memory_conservatism, n_of_immigrants):
 	songModel(filename=filename, FGS=FGS, MGS=MGS, modelMode = modelMode, memory_conservatism = memory_conservatism, n_of_immigrants = n_of_immigrants)
 	return 1
 
-modelMods = ['revolution'] 
+modelMods = ['novelty','revolution'] 
 memory_conservatisms = [0.1,0.5,0.9]
-densities = [0.001,0.01,0.1,1,5]
+densities = [0.001,0.01,0.1,1,5,10]
 n_of_immigrants = [1,5,10]
 
 
@@ -24,12 +24,12 @@ for x in range(0,100):
 		for memory_conservatism in memory_conservatisms:
 			for n_of_immigrant in n_of_immigrants:
 				for density in densities:
-					expNum = f'ABM2_SimN_{x}_' + f'{modelMod}_' + f'c_{memory_conservatism}_' + f'n_imm_{n_of_immigrant}_' + f'd_{density}_'
+					expNum = f'ABM3_SimN_{x}_' + f'{modelMod}_' + f'c_{memory_conservatism}_' + f'n_imm_{n_of_immigrant}_' + f'd_{density}_'
 					MGS = math.sqrt((20)/(density*math.pi))
 					FGS = math.sqrt((20+n_of_immigrant)/(density*math.pi))
 					exp.append((expNum, FGS, MGS, modelMod, memory_conservatism, n_of_immigrant))
 
 if __name__ == '__main__':
-	pool = mp.Pool(processes=48) # here you can change the number of cores
+	pool = mp.Pool(processes=64) # here you can change the number of cores
 	pool.starmap(use_songModel, exp)
 	pool.close()
